1de75eab4321d97b1454fca0343f06e345a6f4d6
luwenbin <linhansong1008@gmail.com>
2026-03-31 14:03:05 +0800

    feat: 增强 RSS 生成逻辑，添加 XML 转义处理

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -9,6 +9,19 @@ const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://zeroanon.com";
 const postsDirectory = join(process.cwd(), "/src/page");
 const EXCLUDE_DIRS = ["friends"];
 
+const escapeXml = (str: string) =>
+  str.replace(
+    /[<>&'"]/g,
+    (c) =>
+      ({
+        "<": "&lt;",
+        ">": "&gt;",
+        "&": "&amp;",
+        "'": "&apos;",
+        '"': "&quot;",
+      })[c]!
+  );
+
 export async function GET() {
   const dirs = fs
     .readdirSync(postsDirectory, { withFileTypes: true })
@@ -24,29 +37,28 @@ export async function GET() {
   const rssItems = allPosts
     .slice(0, 20)
     .map((post) => {
-      const postUrl = `${SITE_URL}/${post.slug}`;
+      const postUrl = `${SITE_URL}/${post.slug.replace(/\/index$/, "")}`;
       const pubDate = new Date(post.date).toUTCString();
-
       return `
     <item>
       <title><![CDATA[${post.title}]]></title>
-      <link>${postUrl}</link>
-      <guid isPermaLink="true">${postUrl}</guid>
+      <link>${escapeXml(postUrl)}</link>
+      <guid isPermaLink="true">${escapeXml(postUrl)}</guid>
       <pubDate>${pubDate}</pubDate>
-      <description><![CDATA[${(post as { description?: string }).description || ""}]]></description>
+      <description><![CDATA[${post.content?.slice(0, 500) || ""}]]></description>
     </item>`;
     })
     .join("");
 
   const rssXml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
   <channel>
     <title>${MATE_TITLE}</title>
-    <link>${SITE_URL}</link>
+    <link>${escapeXml(SITE_URL)}</link>
     <description>博客订阅</description>
     <language>zh-CN</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    <atom:link href="${SITE_URL}/rss.xml" rel="self" type="application/rss+xml" />
+    <atom:link href="${escapeXml(SITE_URL)}/rss.xml" rel="self" type="application/rss+xml" />
     ${rssItems}
   </channel>
 </rss>`;
